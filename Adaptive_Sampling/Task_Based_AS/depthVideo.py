4d7431f907728b73a5fafd7c4309bd3a88a8d975
Jack Maxwell <Jackmaxwell999@gmail.com>
2021-03-08 11:52:39 +0000

    Trimmed some of the fat in the program as the same code was used twice & made the pathing declearation more modular at the top of the file

--- Adaptive_Sampling/Task_Based_AS/depthVideo.py
+++ Adaptive_Sampling/Task_Based_AS/depthVideo.py
@@ -5,9 +5,17 @@ import sys
 import math
 import scipy.io
 
+#Change directory for Matlab Script
+baseName = os.path.basename(__file__)
+dir = __file__
+dir = dir[:-(len(baseName))]
+
+os.chdir(dir)
 initialDir = os.path.abspath(os.getcwd())
+print(initialDir)
 os.chdir('../../')
 projectDir = os.path.abspath(os.getcwd())
+print(projectDir)
 
 #Import Libraries
 sys.path.append(os.path.join(initialDir, "YOLO"))
@@ -19,6 +27,12 @@ import Distribution_Utils as utils
 import Met_Hastings as M_H
 import Random_Sampling as R_S
 import Uniform_Sampling as U_S
+
+# Make Definitions
+boundingBox = 0
+instanceSegmentation = 1
+randomSampling = 2
+uniformSampling = 3
 
 def savePoints(fileName, array):
     frames, dim, length = array.shape
@@ -32,10 +46,11 @@ def savePoints(fileName, array):
             scipy.io.savemat(f, {name: (x, y, z)})
     return 0
 
-def videoDetection(inputRGBVideoPath, inputDepthVideoPath, outputDepthPath, outputRecogPath,detectionType, pointCloud,pixels):
+def videoDetection(inputRGBVideoPath, inputDepthVideoPath, outputDepthPath, outputRecogPath, pixels, detectionType, pointCloud, displayOutput):
     # detectionType   0 - Bounding box
     #                 1 - Instance segmentation
 
+    #Thresholds for detection
     cThresh = 0.5
     oThresh = 0.3
 
@@ -44,8 +59,8 @@ def videoDetection(inputRGBVideoPath, inputDepthVideoPath, outputDepthPath, outp
     capRGB = cv2.VideoCapture(inputRGBVideoPath)
     capDepth = cv2.VideoCapture(inputDepthVideoPath)
 
-    iter = 1
 
+    #Check if the input RGB video and depth video is opened properly
     if (capRGB.isOpened() == False) or (capDepth.isOpened() == False):
         print("Error opening video stream or file")
         return
@@ -68,94 +83,74 @@ def videoDetection(inputRGBVideoPath, inputDepthVideoPath, outputDepthPath, outp
 
     while successRGB and successDepth:
         depth = cv2.cvtColor(depth, cv2.COLOR_RGB2GRAY)
-        if detectionType == 0:
+
+        # Fix for when pixels and pUsed don't match up for saving pClouds
+        pUsed = utils.nonNan(U_S.uniformS(depth, pixels))
+        # End Fix
+
+        if detectionType == boundingBox:
             # Bounding Box
             detected, boundingROI, outputRecog = getYOLOPredsImg(img, cThresh, oThresh, cuda)
-            if detected:
-                outputDepth = M_H.RandomWalkMetHastingsBBox(depth, boundingROI, pixels, 1, 10, 100, 25)
-            else:
-                #Fix for when pixels and pUsed dont match up for saving pClouds
-                pUsed = utils.nonNan(U_S.uniformS(depth, pixels))
-                #End Fix
 
+            if detected:
+                #outputDepth = M_H.RandomWalkMetHastingsBBox(depth, boundingROI, pixels, 1, 10, 100, 25)
+                outputDepth = M_H.RandomWalkMetHastingsBBox(depth, boundingROI, pixels, 1, 10, 1000, 25)
+            else:
                 outputDepth = R_S.randomS(depth, pUsed)
                 outputRecog = img
 
-            if pointCloud:
-                pUsed = utils.nonNan(outputDepth)
-                x, y, z = utils.seperateArrayPC(outputDepth, pUsed)
-                fileName = 'outputBB.mat'
-                if initial:
-                    frames = np.zeros((1, 3, pUsed))
-                    frames[0] = x, y, z
-                    initial = False
-                else:
-                    newDim = np.vstack([x, y, z])
-                    newDim = newDim[np.newaxis, :, :]
-                    frames = np.append(frames, newDim, axis=0)
-
-        elif detectionType == 1:
+        elif detectionType == instanceSegmentation:
             # Instance
             detected, instanceROI, instanceMasks, outputRecog = getMRCNNPredsImg(img)
-
             if detected:
                 # Compress Masks
                 instanceMask = utils.combineMasks(instanceMasks)
                 outputDepth =  M_H.RandomWalkMetHastingsInstance(depth, instanceMask, pixels, 1, 10, 1000, 25)
             else:
-                #Fix for when pixels and pUsed dont match up for saving pClouds
-                pUsed = utils.nonNan(U_S.uniformS(depth, pixels))
-                #End Fix
-
+                #If there is no object detected, preform random sampling
                 outputDepth = R_S.randomS(depth, pUsed)
                 outputRecog = img
 
-            if pointCloud:
-                pUsed = utils.nonNan(outputDepth)
-                x, y, z = utils.seperateArrayPC(outputDepth, pUsed)
-                fileName = 'outputInst.mat'
-                if initial:
-                    frames = np.zeros((1, 3, pUsed))
-                    frames[0] = x, y, z
-                    initial = False
-                else:
-                    newDim = np.vstack([x, y, z])
-                    newDim = newDim[np.newaxis, :, :]
-                    frames = np.append(frames, newDim, axis=0)
-
-        elif detectionType == 2:
+        elif detectionType == randomSampling:
             # Random Sampling
             outputDepth = R_S.randomS(depth, pixels)
             outputRecog = img
 
-        elif detectionType == 3:
+        elif detectionType == uniformSampling:
             # Uniform Sampling
             outputDepth = U_S.uniformS(depth, pixels)
             outputRecog = img
 
+        if pointCloud:
+            x, y, z = utils.seperateArrayPC(outputDepth, pUsed)
+            fileName = 'outputBB.mat'
+            if initial:
+                frames = np.zeros((1, 3, pUsed))
+                frames[0] = x, y, z
+                initial = False
+            else:
+                newDim = np.vstack([x, y, z])
+                newDim = newDim[np.newaxis, :, :]
+                frames = np.append(frames, newDim, axis=0)
+
         h, w = outputDepth.shape
 
-        # Values for non NaN points in the array
-        values = np.empty(pixels)
-        # X and Y coordinates of values
-        points = np.empty((pixels, 2))
+        if pointCloud:
+            for i in range(h):
+                for j in range(w):
+                    if math.isnan(outputDepth[i][j]):
+                        outputDepth[i][j] = 0
+            outputDepth = outputDepth.astype(np.uint8)
+        else:
+            outputDepth = utils.nInterp2D(pUsed, outputDepth)
 
-        c = 0
-        for i in range(h):
-            for j in range(w):
-                if math.isnan(outputDepth[i][j]):
-                    outputDepth[i][j] = 0
-
-        outputDepth = outputDepth.astype(np.uint8)
-
-        cv2.imshow("ModelOutput", outputDepth)
-        cv2.imshow("Predictions", outputRecog)
-        key = cv2.waitKey(1) & 0xFF
-        # if the `q` key was pressed, break the loop
-        if key == ord("q"):
-            break
-
-        iter += 1
+        if displayOutput:
+            cv2.imshow("ModelOutput", outputDepth)
+            cv2.imshow("Predictions", outputRecog)
+            key = cv2.waitKey(1) & 0xFF
+            # if the `q` key was pressed, break the loop
+            if key == ord("q"):
+                break
 
         vidDepth.write(outputDepth)
         vidRecog.write(outputRecog)
@@ -163,7 +158,9 @@ def videoDetection(inputRGBVideoPath, inputDepthVideoPath, outputDepthPath, outp
         (successDepth, depth) = capDepth.read()
         (successRGB, img) = capRGB.read()
 
-    savePoints(fileName, frames)
+    if pointCloud:
+        #Save points if there is a point cloud
+        savePoints(fileName, frames)
 
     capDepth.release()
     capRGB.release()
@@ -178,5 +175,10 @@ if __name__ == "__main__":
     inputRGB = 'Input_RGB/Frames_15fps.mp4'
     outputDepthPath = 'Output/depthOutput.avi'
     outputRecogPath = 'Output/recogOutput.avi'
+    detectionMethod = boundingBox
+    pCloud = True
+    pixels = 5000
+    displayOutput = True
+
     os.chdir(initialDir)
-    videoDetection(inputRGB, inputDepth, outputDepthPath, outputRecogPath, 0, True, 5000)
+    videoDetection(inputRGB, inputDepth, outputDepthPath, outputRecogPath, pixels, detectionMethod, pCloud, displayOutput)
